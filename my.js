66c8044f62c371445d33faac0c550d23680eda2c
Tuomas <tuomas.puro@esedu.fi>
2019-11-18 16:02:24 +0200

    mobiilityylien lisäys

--- my.js
+++ my.js
@@ -58,3 +58,13 @@ function calculateRate(){
 
 
 }
+
+
+function hideMenu() {
+  var x = document.getElementById("menu");
+  if (x.className === "top-menu") {
+    x.className += " responsive-menu";
+  } else {
+    x.className = "top-menu";
+  }
+}
